55f3fbc7d9bc9f61612f6e4e4aa8c4539b4019e8
Max Georgopoulos <max123358@gmail.com>
2020-12-27 16:04:03 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      'inter': ['Inter, ...']
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
